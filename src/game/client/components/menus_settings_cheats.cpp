bc433644076b8eaa7b7f3a99b8e1a257ed88bb32
Batu <139762067+AhmetOglum@users.noreply.github.com>
2026-08-09 12:09:38 +0000

    test6

--- src/game/client/components/menus_settings_cheats.cpp
+++ src/game/client/components/menus_settings_cheats.cpp
@@ -5,31 +5,20 @@
 
 void CMenus::RenderCheats(CUIRect MainView)
 {
-	// CHEATS sekmesinin içinde boşluk bırak
 	MainView.HSplitTop(20.0f, 0, &MainView);
 
-	// "Aimbot" başlığı (kalın harflerle)
 	MainView.HSplitTop(30.0f, &MainView, 0);
 	Ui()->DoLabel(&MainView, Localize("Aimbot"), 24.0f, TEXTALIGN_LEFT);
 	MainView.HSplitTop(20.0f, 0, &MainView);
 
-	// Sensitivite ayarı (1-360 arası kaydırma çubuğu)
-	char aBuf[64];
-	str_format(aBuf, sizeof(aBuf), "%s: %d", Localize("Sensitivity"), g_Config.m_ClAimbotSensitivity);
-	MainView.HSplitTop(20.0f, &MainView, 0);
-	Ui()->DoLabel(&MainView, aBuf, 16.0f, TEXTALIGN_LEFT);
-	MainView.HSplitTop(5.0f, 0, &MainView);
-
 	MainView.HSplitTop(20.0f, &MainView, 0);
 	static CButtonContainer s_SensitivityScroll;
-	g_Config.m_ClAimbotSensitivity = DoScrollbarOption(&s_SensitivityScroll, &MainView, g_Config.m_ClAimbotSensitivity, 1, 360);
+	Ui()->DoScrollbarOption(&g_Config.m_ClAimbotSensitivity, &g_Config.m_ClAimbotSensitivity, &MainView, Localize("Sensitivity"), 1, 360);
 	MainView.HSplitTop(20.0f, 0, &MainView);
 
-	// Sol ve sağ olarak ikiye böl
 	CUIRect Left, Right;
 	MainView.VSplitMid(&Left, &Right, 20.0f);
 
-	// Sağ tarafta "Enabled" checkbox'ı (X işareti)
 	Right.HSplitTop(20.0f, &Right, 0);
 	static CButtonContainer s_EnabledToggle;
 	if(DoButton_CheckBox(&s_EnabledToggle, Localize("Enabled"), g_Config.m_ClAimbotEnabled, &Right))
@@ -38,7 +27,6 @@ void CMenus::RenderCheats(CUIRect MainView)
 	}
 	Right.HSplitTop(20.0f, 0, &Right);
 
-	// Sol tarafta, sensitivitenin altında "Draw Lines" checkbox'ı
 	Left.HSplitTop(40.0f, 0, &Left);
 	Left.HSplitTop(20.0f, &Left, 0);
 	static CButtonContainer s_DrawLinesToggle;
