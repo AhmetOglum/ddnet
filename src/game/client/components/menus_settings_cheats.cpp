579a2c59ef458ed11e05abbebfc2a6c4ebc63993
Batu <139762067+AhmetOglum@users.noreply.github.com>
2026-08-09 15:29:47 +0000

    test13

--- src/game/client/components/menus_settings_cheats.cpp
+++ src/game/client/components/menus_settings_cheats.cpp
@@ -5,29 +5,29 @@
 
 void CMenus::RenderCheats(CUIRect MainView)
 {
-	MainView.HSplitTop(20.0f, 0, &MainView);
+	MainView.HSplitTop(20.0f, nullptr, &MainView);
 
-	MainView.HSplitTop(30.0f, &MainView, 0);
+	MainView.HSplitTop(30.0f, &MainView, nullptr);
 	Ui()->DoLabel(&MainView, Localize("Aimbot"), 24.0f, TEXTALIGN_LEFT);
-	MainView.HSplitTop(20.0f, 0, &MainView);
+	MainView.HSplitTop(20.0f, nullptr, &MainView);
 
-	MainView.HSplitTop(20.0f, &MainView, 0);
+	MainView.HSplitTop(20.0f, &MainView, nullptr);
 	Ui()->DoScrollbarOption(&g_Config.m_ClAimbotSensitivity, &g_Config.m_ClAimbotSensitivity, &MainView, Localize("Sensitivity"), 1, 360);
-	MainView.HSplitTop(20.0f, 0, &MainView);
+	MainView.HSplitTop(20.0f, nullptr, &MainView);
 
 	CUIRect Left, Right;
 	MainView.VSplitMid(&Left, &Right, 20.0f);
 
-	Right.HSplitTop(20.0f, &Right, 0);
+	Right.HSplitTop(20.0f, &Right, nullptr);
 	static CButtonContainer s_EnabledToggle;
 	if(DoButton_CheckBox(&s_EnabledToggle, Localize("Enabled"), g_Config.m_ClAimbotEnabled, &Right))
 	{
 		g_Config.m_ClAimbotEnabled ^= 1;
 	}
-	Right.HSplitTop(20.0f, 0, &Right);
+	Right.HSplitTop(20.0f, nullptr, &Right);
 
-	Left.HSplitTop(40.0f, 0, &Left);
-	Left.HSplitTop(20.0f, &Left, 0);
+	Left.HSplitTop(40.0f, nullptr, &Left);
+	Left.HSplitTop(20.0f, &Left, nullptr);
 	static CButtonContainer s_DrawLinesToggle;
 	if(DoButton_CheckBox(&s_DrawLinesToggle, Localize("Draw Lines"), g_Config.m_ClAimbotDrawLines, &Left))
 	{
