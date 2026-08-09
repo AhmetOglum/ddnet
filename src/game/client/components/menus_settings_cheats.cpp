539017483fd27be63502911bedf22447297f3d93
Batu <139762067+AhmetOglum@users.noreply.github.com>
2026-08-09 16:33:26 +0000

    test16

--- src/game/client/components/menus_settings_cheats.cpp
+++ src/game/client/components/menus_settings_cheats.cpp
@@ -13,7 +13,7 @@ void CMenus::RenderCheats(CUIRect MainView)
 
     MainView.HSplitTop(20.0f, &MainView, 0);
     static CButtonContainer s_SensitivityScroll;
-    Ui()->DoScrollbarOption(&s_SensitivityScroll, &g_Config.m_ClAimbotSensitivity, &MainView, Localize("Sensitivity"), 1, 360);
+    Ui()->DoScrollbarOption(&s_SensitivityScroll, &g_Config.m_ClAimbotSensitivity, &MainView, Localize("Fov"), 1, 360);
     MainView.HSplitTop(20.0f, 0, &MainView);
 
     CUIRect Left, Right;
